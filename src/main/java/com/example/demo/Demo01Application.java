18ad4bf97db881a74664e0b29263478e54e2ab81
tv4nh <tva8320@gmail.com>
2021-12-24 15:47:56 +0700

    push demo

---
+++ src/main/java/com/example/demo/Demo01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication   // đánh dấu nơi khởi chạy chương trình springboot
+public class Demo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo01Application.class, args);
+	}
+
+}
